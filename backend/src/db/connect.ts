d0feb5d532df2f8cae7535d5d4770073f6603b40
Chandan Chaudhary <chandankkumar156@gmail.com>
2026-02-25 16:28:41 +0530

    fix: update MongoDB connection to use process.env for URI

--- backend/src/db/connect.ts
+++ backend/src/db/connect.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
-import { env } from "../config/env.js";
 
 export async function connectDb() {
   mongoose.set("strictQuery", true);
-  await mongoose.connect(env.MONGODB_URI);
+  await mongoose.connect(process.env.MONGODB_URI as string);
 }
